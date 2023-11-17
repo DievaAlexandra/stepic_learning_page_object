02c9ec9196a68a9d92615633f2003ffec38fa9bc
aleksandra dieva <dieva.sro@mail.ru>
2023-11-17 20:04:19 +0300

    Base page and Main page are added

--- test_main_page.py
+++ test_main_page.py
@@ -3,8 +3,16 @@ from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+# открыть страницу логина
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+# проверка пользователь видит страницу логина
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
