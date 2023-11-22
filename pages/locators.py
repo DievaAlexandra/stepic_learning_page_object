00d965dfd8d35a4cc4f0cf0f08756de43d71dc48
aleksandra dieva <dieva.sro@mail.ru>
2023-11-22 20:59:48 +0300

    UPDATES

---
+++ pages/locators.py
@@ -0,0 +1,14 @@
+# ЗДЕСЬ ОПИСАНЫ ВСЕ СЕЛЕКТОРЫ
+
+
+from selenium.webdriver.common.by import By
+
+
+# каждый класс соответствует каждому классу PageObject
+# каждый селектор это пара. как искать и что искать
+class MainPageLocators():
+    LOGIN_LINK = (By.ID, "login_link")
+
+class LoginPageLocators():
+    REGISTER_FORM =
+
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,16 @@
 # ГЛАВНАЯ СТРАНИЦА САЙТА
 
 from selenium.webdriver.common.by import By
-from pages.base_page import \
-    BasePage  # наследование от BasePage  и теперь можем тут использовать всё что есть в классе предке
+from pages.base_page import BasePage  # наследование от BasePage  и теперь можем тут использовать всё что есть в классе предке
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):  # нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
+    #здесь проверка  на поиск наличие элемента на странице
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" #обращаем внимение на символ *, он означает
+                                                                                                    # что передали именно пару и этот кортеж нужно распаковать
